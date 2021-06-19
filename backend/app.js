ac8c7fe7fe23646d44bb51819848c27a4674f1cb
Jeffrey Wang <wangj2840@gmail.com>
2021-06-19 13:16:03 -0700

    Added some schema rules

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express(); 
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser'); 
 require("dotenv/config")
 
 //Port Number
@@ -10,9 +11,11 @@ app.listen(3000);
 
 //Reference specific route
 const exampleRoute = require('./routes/exampleRoute'); 
+const bodyParser = require('body-parser');
 
 //Example of running middleware on routes
 app.use('/', exampleRoute);
+app.use(bodyParser.json());
 
 //Connect to DB 
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser}, 
