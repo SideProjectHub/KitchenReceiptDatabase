01960f9fd1d604aef0ac6cc6a3960fa7ba387e40
Jeffrey Wang <wangj2840@gmail.com>
2021-06-23 15:58:37 -0700

    Added Menu options to fridge Icon

--- frontend/project/lib/Screens/FoodPage/Components/fridge_menu.dart
+++ frontend/project/lib/Screens/FoodPage/Components/fridge_menu.dart
@@ -14,8 +14,8 @@ class _FridgeMenuState extends State<FridgeMenu>
   @override
   void initState() {
     super.initState();
-    controller =
-        AnimationController(duration: Duration(milliseconds: 900), vsync: this);
+    controller = AnimationController(
+        duration: Duration(milliseconds: 1000), vsync: this);
   }
 
   @override
@@ -32,16 +32,41 @@ class RadialAnimation extends StatelessWidget {
         ).animate(
           CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn),
         ),
+        translation = Tween<double>(
+          begin: 0.0,
+          end: 150.0,
+        ).animate(CurvedAnimation(
+          parent: controller,
+          curve: Curves.elasticOut,
+        )),
         super(key: key);
 
   final AnimationController controller;
   final Animation<double> scale;
+  final Animation<double> translation;
 
   build(context) {
     return AnimatedBuilder(
         animation: controller,
         builder: (context, builder) {
           return Stack(alignment: Alignment.center, children: [
+            _buildButton(330,
+                color: Colors.red,
+                icon: Image.asset('assets/icons/fruits.png')),
+            _buildButton(30,
+                color: Colors.green,
+                icon: Image.asset('assets/icons/veggies.png')),
+            _buildButton(90,
+                color: Colors.brown,
+                icon: Image.asset('assets/icons/meat.png')),
+            _buildButton(150,
+                color: Colors.grey, icon: Image.asset('assets/icons/misc.png')),
+            _buildButton(210,
+                color: Colors.yellow,
+                icon: Image.asset('assets/icons/grains.png')),
+            _buildButton(270,
+                color: Colors.blue,
+                icon: Image.asset('assets/icons/diary.png')),
             Transform.scale(
               scale: scale.value - 1.5,
               child: new IconButton(
@@ -60,11 +85,32 @@ class RadialAnimation extends StatelessWidget {
         });
   }
 
+  _buildButton(double angle, {required Color color, required Image icon}) {
+    final double rad = radians(angle);
+    return Transform(
+      transform: Matrix4.identity()
+        ..translate(
+          (translation.value) * cos(rad),
+          (translation.value) * sin(rad),
+        ),
+      child: Center(
+          child: new Ink(
+        decoration: new ShapeDecoration(
+          color: color,
+          shape: CircleBorder(),
+        ),
+        child: new IconButton(
+            icon: icon, iconSize: 50, color: Colors.white, onPressed: _close),
+      )),
+    );
+  }
+
   _open() {
     controller.forward();
   }
 
   _close() {
+    print("Hello");
     controller.reverse();
   }
 }
