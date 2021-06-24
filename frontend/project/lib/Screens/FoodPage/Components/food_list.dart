94c8f84c3d7ee23542390927d5558cbbb0e81d21
Jeffrey Wang <wangj2840@gmail.com>
2021-06-24 14:44:08 -0700

    Create simple List component to display List of foods

--- frontend/project/lib/Screens/FoodPage/Components/food_list.dart
+++ frontend/project/lib/Screens/FoodPage/Components/food_list.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+/*Food List */
+/************************************************* */
 class FoodList extends StatefulWidget {
   const FoodList({Key? key, required this.foods}) : super(key: key);
 
   //Takes in list of foods
-  final List<Food> foods;
+  List<Food> foods;
 
   @override
   _FoodListState createState() => _FoodListState();
@@ -12,21 +14,56 @@ class FoodList extends StatefulWidget {
 
 class _FoodListState extends State<FoodList> {
   @override
+
+  void _handleFoodList(Food food){ 
+    setState(() { 
+      widget.foods.remove(food); 
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      children: [ 
+        //title here 
+        ListView( 
+          padding: EdgeInsets.symmetric(vertical: 8.0), 
+          children: widget.foods.map((Food food)) { 
+            return FoodListItem( 
+              food: food, 
+              onFoodListChange: _handleFoodList,
+            );
+          },
+        ), 
+      ],
+    );
   }
 }
 
+typedef void FoodChanged(Food food); 
+
+/*UI for each individual Food*/
+/************************************************* */
 class FoodListItem extends StatelessWidget {
-  const FoodListItem({Key? key}) : super(key: key);
+  FoodListItem({
+    required this.food, 
+    required this.onFoodListChange,
+  }) : super(key: ObjectKey(food));
+
+  final Food food; 
+  final FoodChanged onFoodListChange; 
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Listview( 
+      title: Text(product.name), 
+    ); 
   }
 }
 
+/*Specific Food Class*/
+/************************************************* */
 class Food {
   const Food({required this.name});
   final String name;
+  //@todo: Additional attributes maybe added here (e.g. quantity)
 }
