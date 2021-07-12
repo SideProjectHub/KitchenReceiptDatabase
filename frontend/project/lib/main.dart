340c4ee2ec2aed57c13f88fbe3b4cab61dd68da2
Jeffrey Wang <wangj2840@gmail.com>
2021-07-12 15:59:10 -0700

    Added custum appbar

--- frontend/project/lib/main.dart
+++ frontend/project/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project/widgets/profile_pic.dart';
 import 'Screens/FoodPage/user_food_page.dart';
+import 'widgets/custom_app_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +11,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'FoodPage', home: UserFoodPage());
+    return MaterialApp(
+      title: "Hello",
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: Colors.black,
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        ),
+      ),
+      home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Builder(builder: (BuildContext context) {
+              return CustomAppBar(
+                leading: const Text(""),
+                trailing: const Text(""),
+                title: "Profile",
+                childHeight: 100,
+                height: 225,
+                isBig: true,
+                child: ProfilePic(diameter: 100),
+              );
+            }),
+            const Text("Hello World"),
+          ],
+        ),
+      ),
+    );
+    // return MaterialApp(title: 'FoodPage', home: UserFoodPage());
   }
 }
---
+++ frontend/project/lib/widgets/custom_app_bar.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final Widget leading, trailing, child;
+  final double height, childHeight;
+  final String title;
+  final bool isBig;
+
+  const CustomAppBar({
+    Key? key,
+    required this.leading,
+    required this.trailing,
+    required this.title,
+    required this.childHeight,
+    required this.height,
+    required this.isBig,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          clipper: _AppBarClipper(
+            isBig: isBig,
+            childHeight: childHeight,
+          ),
+          child: Container(
+            padding: const EdgeInsets.only(top: 32),
+            color: Theme.of(context).primaryColor,
+            height: height,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                leading,
+                FlatButton(
+                  onPressed: null,
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                    ),
+                  ),
+                ),
+                trailing,
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: childHeight / 2,
+          left: 0,
+          right: 0,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: isBig ? child : Container(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _AppBarClipper extends CustomClipper<Path> {
+  final bool isBig;
+  final double childHeight;
+
+  const _AppBarClipper({
+    required this.isBig,
+    required this.childHeight,
+  });
+
+  @override
+  Path getClip(Size size) {
+    double height = isBig ? size.height - childHeight : size.height;
+
+    Path path = Path();
+
+    path.moveTo(0, height - 40);
+    path.quadraticBezierTo(size.width / 2, height, size.width, height - 40);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
