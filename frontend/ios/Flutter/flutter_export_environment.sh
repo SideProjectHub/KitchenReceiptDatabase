af131a32b7704478877c96e88b78114a36a6db35
Jeffrey Wang <wangj2840@gmail.com>
2021-07-22 18:34:18 -0700

    Updated gitignore

--- frontend/ios/Flutter/flutter_export_environment.sh
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Joey\Desktop\login_page\login_page"
+export "FLUTTER_ROOT=F:\flutter-win"
+export "FLUTTER_APPLICATION_PATH=F:\Project\KitchenReceiptDatabase\frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
