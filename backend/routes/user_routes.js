e92d988e952ede799c0179408cc38bc762952249
Jeffrey Wang <wangj2840@gmail.com>
2021-07-26 15:29:25 -0700

    Skeleton code

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const user_router = express.Router(); 
 const User = require('../schemas/userSchema');
 
+//TODO Returns information needed for the profile page
+user_router.get('/getProfile', (req, res) => { 
+
+});
 
 user_router.post('/addUser', (req, res) => { 
     console.log(req.body); 
