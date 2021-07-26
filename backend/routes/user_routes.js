8b0f44266c54dc4798fb72408828a61a7fd63983
Jeffrey Wang <wangj2840@gmail.com>
2021-07-26 16:40:23 -0700

    Created GET Profile Page API

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -2,11 +2,23 @@ const express = require('express');
 const user_router = express.Router(); 
 const User = require('../schemas/userSchema');
 
-//TODO Returns information needed for the profile page
-user_router.get('/getProfile', (req, res) => { 
+//Gets User profile with UID needed as parameter
+user_router.get('/getProfile/:uid', (req, res) => { 
+    var uid = req.params.uid;  
+    if (uid == null || uid == undefined){ 
+        res.status(400).send('Missing User UID')
+        return; 
+    }
 
+    User.find({uid: uid}, '-_id fridgeTotal foodTotal eatenTotal')
+    .then(user => { 
+        res.send(user[0]); 
+    }).catch(err => {
+        res.status(400).send('UserProfile currently unavailable ' + err);
+    });
 });
 
+//Adds new User with Firebase credentials. 
 user_router.post('/addUser', (req, res) => { 
     console.log(req.body); 
     const user = new User({ 
--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -6,8 +6,10 @@ const UserSchema = mongoose.Schema(
     uid: {type: String, required: true, unique: true}, 
     email:{type: String, required: true, unique: true}, 
     displayName:{type: String, required: true, unique: false},
-    fridgeList: [{fridge: {type: mongoose.Schema.Types.ObjectId, unique: true}}] 
-
+    fridgeList: [{fridge: {type: mongoose.Schema.Types.ObjectId, unique: true}}],  
+    fridgeTotal: {type: Number, default: 0},
+    foodTotal: {type: Number, default: 0},
+    eatenTotal: {type: Number, default: 0}
     },
     { collection: 'users'}
 );
