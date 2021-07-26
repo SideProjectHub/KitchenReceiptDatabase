72f1995da8bdf85eb61b9d89cc1a2d0d4f7fe1ec
Jeffrey Wang <wangj2840@gmail.com>
2021-07-26 14:54:53 -0700

    Created fridge Schema

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -6,9 +6,10 @@ const UserSchema = mongoose.Schema(
     uid: {type: String, required: true, unique: true}, 
     email:{type: String, required: true, unique: true}, 
     displayName:{type: String, required: true, unique: false},
-    fridge: {type: [], required: true} 
+    fridgeList: [{fridge: {type: mongoose.Schema.Types.ObjectId, unique: true}}] 
+
     },
     { collection: 'users'}
-    );
+);
 
 module.exports = mongoose.model('UserSchema', UserSchema);
